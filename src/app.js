3cfac0eaff3f8a62313f03b33a14942114488e8a
Laura-prog-ui <duarteriveralauraestefania0@gmail.com>
2023-02-08 15:37:37 -0500

    Commit-10

--- src/app.js
+++ src/app.js
@@ -1,39 +1,39 @@
-<<<<<<< HEAD
+
 import React, { useState } from 'react';
 import Form from './components/Form';
 import Section from './components/Section';
 import List from './components/List';
-=======
+
 import React from "react";
 import Form from "./components/Form";
 import Section from "./components/Section";
 import List from "./components/List"
->>>>>>> 8f4778ac6b2eb30e812ca56f0491e4d91c3f3587
+
 
 const APP_TITLE = 'TO-DO-APP';
 const list = [
-  { title: 'test #1', completed: false },
-  { title: 'test #2', completed: false },
-  { title: 'test #3', completed: false },
+    { title: 'test #1', completed: false },
+    { title: 'test #2', completed: false },
+    { title: 'test #3', completed: false },
 ];
 
 const App = () => {
-<<<<<<< HEAD
-  const [todoList, setTodoList] = useState(list);
-  return (
+
+const [todoList, setTodoList] = useState(list);
+return (
     <div className="ui container center aligned">
-      <Section>
-        <h1>{APP_TITLE}</h1>
-      </Section>
-      <Section>
-        <Form />
-      </Section>
-      <section>
-        <List list={todoList} />
-      </section>
+        <Section>
+            <h1>{APP_TITLE}</h1>
+        </Section>
+        <Section>
+            <Form />
+        </Section>
+        <section>
+            <List list={todoList} />
+        </section>
     </div>
-  );
-=======
+);
+
     return (
         <div className="ui container center aligned">
             <Section>
@@ -45,7 +45,6 @@ const App = () => {
             </Section>
         </div>
     );
->>>>>>> 8f4778ac6b2eb30e812ca56f0491e4d91c3f3587
 };
 
 export default App;
