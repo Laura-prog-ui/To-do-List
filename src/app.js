74dbfddc488dab7741128c70a5be2d13ce1a36aa
Andreinavlqz <andreinafloresvlqz54@gmail.com>
2023-02-08 15:42:04 -0500

    arreglos

--- src/app.js
+++ src/app.js
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
 import React, { useState } from 'react';
 import Form from './components/Form';
 import Section from './components/Section';
 import List from './components/List';
-=======
-import React from "react";
-import Form from "./components/Form";
-import Section from "./components/Section";
-import List from "./components/List"
->>>>>>> 8f4778ac6b2eb30e812ca56f0491e4d91c3f3587
 
 const APP_TITLE = 'TO-DO-APP';
 const list = [
@@ -18,7 +11,6 @@ const list = [
 ];
 
 const App = () => {
-<<<<<<< HEAD
   const [todoList, setTodoList] = useState(list);
   return (
     <div className="ui container center aligned">
@@ -33,19 +25,6 @@ const App = () => {
       </section>
     </div>
   );
-=======
-    return (
-        <div className="ui container center aligned">
-            <Section>
-                <h1>{APP_TITLE}</h1>
-            </Section>
-            <Section>
-                <Form />
-                <List/>
-            </Section>
-        </div>
-    );
->>>>>>> 8f4778ac6b2eb30e812ca56f0491e4d91c3f3587
 };
 
 export default App;
