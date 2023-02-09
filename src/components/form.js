034f547fba94534c6a63af69b143ec0544f5c687
Laura-prog-ui <duarteriveralauraestefania0@gmail.com>
2023-02-09 08:32:19 -0500

    Commit-11

--- src/components/form.js
+++ src/components/form.js
@@ -1,32 +1,34 @@
 import React, { useState } from "react";
+
 const Form = () => {
-const [inputValue, setInputValue] = useState("");
-const handleInputChange = (e) => {
-setInputValue(e.target.value);
+
+    const [inputValue, setInputValue] = useState("");
+    const handleInputChange = (e) => {
+        setInputValue(e.target.value);
 };
 const handleFormSubmit = (e) => {
-e.preventDefault();
+    e.preventDefault();
 // Aquí puedes agregar la lógica para manejar la sumisión del formulario
 };
 return (
-<form className="ui form" onSubmit={handleFormSubmit}>
-<div className="ui grid center aligned">
-<div className="row">
-<div className="column five wide">
-<input
-value={inputValue}
-onChange={handleInputChange}
-type="text"
-placeholder="Enter something to do..."
-/>
-</div>
-<div className="column one wide">
-<button type="submit" className="ui button circular icon green">
-<i className="white plus icon"></i></button>
-</div>
-</div>
-</div>
-</form>
+    <form className="ui form" onSubmit={handleFormSubmit}>
+        <div className="ui grid center aligned">
+            <div className="row">
+                <div className="column five wide">
+                    <input
+                    value={inputValue}
+                    onChange={handleInputChange}
+                    type="text"
+                    placeholder="Enter something to do..."/>
+                </div>
+                <div className="column one wide">
+                    <button type="submit" className="ui button circular icon green">
+                        <i className="white plus icon"></i>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </form>
 );
 };
 export default Form;
