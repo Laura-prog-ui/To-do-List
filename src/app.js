d9976103f393f8e1d17eb2b28ad48b9b4b964f0a
Yochi22 <116101499+Yochi22@users.noreply.github.com>
2023-02-10 13:15:27 -0600

    commit

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+
 import React, { useState } from 'react';
 import Form from './components/Form';
 import Section from './components/Section';
@@ -5,15 +6,10 @@ import List from './components/List';
 import "./style.css"
 
 
-const app_title = 'TO-DO-APP';
-const list = [
-    { title: 'test #1', completed: false },
-    { title: 'test #2', completed: false },
-    { title: 'test #3', completed: false },
-];
+const app_title = 'To-Do-App';
 
 const TodoApp = () => {
-  const [todoList, setTodoList] = useState(list);
+  const [todoList, setTodoList] = useState([]);
   const addTodo = (todo) => {
     setTodoList([...todoList, todo]);
   };
